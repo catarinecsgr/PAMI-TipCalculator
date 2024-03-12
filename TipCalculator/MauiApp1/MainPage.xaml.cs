4e3b414aebb9b351bba998db7b56467311f25d39
Catarine <catcas08@gmail.com>
2024-03-11 22:48:03 -0300

    feat: adicionei métodos de calcular, arredondar e definir porcentagem

---
+++ TipCalculator/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnFifteenPercentMethod(object sender, EventArgs e)
+        {
+            SliderTipPercent.Value = 15;
+            double tip = CalculateTip();
+            double amount = Double.Parse(AmountEntry.Text.ToString());
+            double totalValue = tip + amount;
+
+            TotalTip.Text = $"R$ {tip.ToString()}";
+            AmountTotal.Text = $"R$ {totalValue.ToString()}";
+        }
+
+        private void OnTwentyPercentMethod(object sender, EventArgs e)
+        {
+            SliderTipPercent.Value = 20;
+            double tip = CalculateTip();
+            double amount = Double.Parse(AmountEntry.Text.ToString());
+            double totalValue = tip + amount;
+
+            TotalTip.Text = $"R$ {tip.ToString()}";
+            AmountTotal.Text = $"R$ {totalValue.ToString()}";
+        }
+
+        private void OnRoundDownMethod(object sender, EventArgs e)
+        {
+            double tip = CalculateTip();
+            double amount = Double.Parse(AmountEntry.Text.ToString());
+            double roundedResult = Math.Floor(tip);
+            double totalValue = roundedResult + amount;
+
+            TipPercentage.Text = $"{roundedResult.ToString()}%";
+            TotalTip.Text = $"R$ {roundedResult.ToString()}";
+            AmountTotal.Text = $"R$ {totalValue.ToString()}";
+        }
+
+        private void OnRoundUpMethod(object sender, EventArgs e)
+        {
+            double tip = CalculateTip();
+            double amount = Double.Parse(AmountEntry.Text.ToString());
+            double roundedResult = Math.Ceiling(tip);
+            double totalValue = roundedResult + amount;
+
+            TipPercentage.Text = $"{roundedResult.ToString()}%";
+            TotalTip.Text = $"R$ {roundedResult.ToString()}";
+            AmountTotal.Text = $"R$ {totalValue.ToString()}";
+        }
+
+        private double CalculateTip()
+        {
+            double percent = SliderTipPercent.Value;
+            double amount = Double.Parse(AmountEntry.Text.ToString());
+            double tip = amount * (percent / 100);
+            return tip;
+        }
+
+        private void OnValueChange(object sender, EventArgs e)
+        {
+            TipPercentage.Text = $"{Math.Round(SliderTipPercent.Value).ToString()}%";
+        }
+    }
+}
